7665119514eaab354ec96f4a00a8e49418e70f43
jephthah <jephthah.mbah@outlook.com>
2024-01-24 13:58:37 +0100

    made create-task page responsive

--- app/(dynamic-pages)/(protected-pages)/dashboard/layout.tsx
+++ app/(dynamic-pages)/(protected-pages)/dashboard/layout.tsx
@@ -22,5 +22,5 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <div className="h-full">{children}</div>;
+  return <div className="">{children}</div>;
 }
