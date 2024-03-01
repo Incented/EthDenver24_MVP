10c827f653055b06463cbd5e78072883ae871547
jephthah <jephthah.mbah@outlook.com>
2024-03-01 19:14:19 +0100

    grant application card UI

--- app/(dynamic-pages)/(protected-pages)/grants/[id]/page.tsx
+++ app/(dynamic-pages)/(protected-pages)/grants/[id]/page.tsx
@@ -1,13 +1,17 @@
 
 
+import { Search } from "@/components/Search";
 import GoBack from "@/components/ui/GoBack";
+import Pagination from "@/components/ui/Pagination";
 import { Typography } from "@/components/ui/Typography";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
 import { getGrantApplicationsByGrantId } from "@/data/user/grant-projects";
 import { getGrantProgramById } from "@/data/user/grants";
 import { parseJsonToStringArray } from "@/lib/utils";
 import { Table } from "@/types";
+import { Filter } from "lucide-react";
 import Link from "next/link";
 import { z } from "zod";
 import { TaskFileArray, filesSchema } from "../../grant-applications/[id]/_components/GrantApplicationDetail";
@@ -53,36 +57,38 @@ export default async function GrantDetailsPage({
 
 
   return (
-    <div className="relative mx-4 my-10 mb-10 sm:mx-8">
+    <div className="relative mx-4 my-4 mb-10 sm:mx-8">
       <GoBack />
-      <Card className=" p-6 border-none shadow-none">
-        <div className="flex flex-col space-y-4 gap-12 lg:flex-row lg:space-y-0 lg:space-x-8">
+      <Card className="p-4 border-none shadow-none ">
+        <div className="flex flex-col gap-4 space-y-4 lg:flex-row lg:space-y-0 lg:space-x-8">
           <div className="flex-1">
             <Typography.H2>{grantProgram.title}</Typography.H2>
-            <Typography.P className="text-sm text-muted-foreground">Managed by: The Arbitrum Foundation</Typography.P>
+            <Typography.P className="text-primary">Active</Typography.P>
+            <Separator className="my-2" />
+            <Typography.P className="text-sm text-muted-foreground">Managed by: {grantProgram.title}</Typography.P>
             <div className="mt-4 space-y-2">
-              <Card className="shadow-none p-4 flex justify-between">
+              <Card className="flex justify-between p-4 shadow-none">
                 <Typography.P className="text-lg font-medium">Grant Pool</Typography.P>
-                <Typography.P className="text-primary text-lg font-semibold">{grantProgram.grant_pool} ARB</Typography.P>
+                <Typography.P className="text-lg font-semibold text-primary">{grantProgram.grant_pool} ARB</Typography.P>
               </Card>
-              <Card className="shadow-none p-4 flex justify-between">
+              <Card className="flex justify-between p-4 shadow-none">
                 <Typography.P className="text-lg font-medium">Prioritization Reward Pool</Typography.P>
-                <Typography.P className="text-primary text-lg font-semibold">{grantProgram.prioritization_reward_percentage! * 4 * 10} ARB</Typography.P>
+                <Typography.P className="text-lg font-semibold text-primary">{grantProgram.prioritization_reward_percentage! * 4 * 10} ARB</Typography.P>
               </Card>
-              <Card className="shadow-none p-4 flex justify-between">
+              <Card className="flex justify-between p-4 shadow-none">
                 <Typography.P className="text-lg font-medium">Slash Percentage</Typography.P>
-                <Typography.P className="text-primary text-lg font-semibold">{grantProgram.slash_percentage} %</Typography.P>
+                <Typography.P className="text-lg font-semibold text-primary">{grantProgram.slash_percentage} %</Typography.P>
               </Card>
             </div>
           </div>
-          <Card className="flex-1 space-y-4 shadow-none border-none bg-muted p-8">
+          <Card className="flex-1 p-8 space-y-4 border-none shadow-none bg-muted">
             <div className="flex flex-col justify-between h-full">
               <div
-                className="prose text-base text-muted-foreground prose-lg prose-slate  dark:prose-invert prose-headings:font-display font-default focus:outline-none max-w-full mb-6"
+                className="max-w-full mb-6 text-base prose prose-lg text-muted-foreground prose-slate dark:prose-invert prose-headings:font-display font-default focus:outline-none"
                 dangerouslySetInnerHTML={{ __html: grantProgram.description as string }}
               />
               <Link href={`/grants/${id}/submit-application`}>
-                <Button className="mt-4 w-full bg-orange-500 text-white py-2 px-4 rounded hover:bg-orange-600">
+                <Button className="w-full px-4 py-2 mt-4 text-white bg-orange-500 rounded hover:bg-orange-600">
                   Apply
                 </Button>
               </Link>
@@ -91,8 +97,19 @@ export default async function GrantDetailsPage({
           </Card>
         </div>
       </Card>
-      <Typography.H3 className="mt-8 mb-6">Grant Applications</Typography.H3>
-      <div className="grid grid-rows-1 gap-4 w-full">
+
+      <div className="items-center md:flex">
+        <Typography.H3 className="mt-4 mb-6">Grant Applications</Typography.H3>
+        <div className="flex items-center gap-3 mt-4 ml-auto md:mt-0">
+          <Search placeholder="Search Application" />
+          <Button>
+            <Filter size={20} className="mr-1" />
+            Filter
+          </Button>
+        </div>
+      </div>
+
+      <div className="grid w-full gap-4">
         {grantApplications.filter(application => application.grant_project_status !== "draft").map((application) => (
           <GrantApplicationCard
             key={application.id}
@@ -104,9 +121,16 @@ export default async function GrantDetailsPage({
             grantProgram={grantProgram}
             grantProjectStatus={application.grant_project_status}
             grantProjectType={parseJsonToStringArray(application.grant_project_types)}
-            isVertical={true}
+            isVertical
           />
         ))}
+      </div>
+      <div className="py-4 ">
+        <Pagination
+          title="Applications"
+          totalPages={10}
+          count={3}
+        />
       </div>
     </div>
   );
