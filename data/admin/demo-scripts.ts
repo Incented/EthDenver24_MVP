d9824076c986b94cb4014b71fdd58bdfe7aa7ce2
psiddharthdesign <107192927+psiddharthdesign@users.noreply.github.com>
2024-03-02 20:26:30 +0530

    Remove unnecessary code in demo-scripts.ts

--- data/admin/demo-scripts.ts
+++ data/admin/demo-scripts.ts
@@ -436,7 +436,6 @@ export const resetGrantApplicationsForNextDemo = async () => {
     },
     {
       name: "ArbQuest - Adventure RPG on Arbitrum",
-      updated_at: "2024-03-02 07:42:57.229",
       organization_id: "379f55af-2377-49c5-8203-600aabcc1cab",
       project_status: "draft",
       description:
