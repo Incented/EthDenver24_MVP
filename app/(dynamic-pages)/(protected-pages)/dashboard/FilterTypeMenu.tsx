22fced22b0f8ce2bb8fee4e8c262dcab02d833a3
psiddharthdesign <107192927+psiddharthdesign@users.noreply.github.com>
2024-03-04 15:56:32 +0530

    add task type filters

--- app/(dynamic-pages)/(protected-pages)/dashboard/FilterTypeMenu.tsx
+++ app/(dynamic-pages)/(protected-pages)/dashboard/FilterTypeMenu.tsx
@@ -5,7 +5,9 @@ import { Popover, PopoverTrigger } from "@/components/ui/popover";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Separator } from "@/components/ui/separator";
 import { PopoverContent } from "@radix-ui/react-popover";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { FC, useState } from "react";
+import urlJoin from "url-join";
 
 interface FilterTypeMenuProps {
   taskTypes: {
@@ -39,18 +41,33 @@ export type task_slug =
 const FilterTypeMenu: FC<FilterTypeMenuProps> = ({ taskTypes }) => {
 
   const [selectedTypes, setSelectedTypes] = useState<task_slug[]>([]);
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  let typeParams = searchParams?.getAll('type') ?? [];
 
   const toggleTypeSelection = (typeName: task_slug): void => {
+
     setSelectedTypes((currentSelected) => {
       const isSelected = currentSelected.includes(typeName);
-      if (isSelected) {
-        return currentSelected.filter((type) => type !== typeName);
-      } else {
-        return [...currentSelected, typeName];
-      }
+      const newSelected = isSelected ? currentSelected.filter((type) => type !== typeName) : [...currentSelected, typeName];
+      return newSelected;
     });
-  };
 
+    const newSearchParams = new URLSearchParams(searchParams?.toString());
+    const currentTypes = newSearchParams.getAll('type');
+
+    if (currentTypes.includes(typeName)) {
+      const filteredTypes = currentTypes.filter((type) => type !== typeName);
+      newSearchParams.delete('type'); // Clear all 'type' entries
+      filteredTypes.forEach((type) => newSearchParams.append('type', type)); // Re-add filtered types
+    } else {
+      newSearchParams.append('type', typeName);
+    }
+
+    const newUrl = urlJoin(pathname ?? '/', `?${newSearchParams.toString()}`);
+    router.push(newUrl);
+  }
 
 
 
@@ -74,7 +91,7 @@ const FilterTypeMenu: FC<FilterTypeMenuProps> = ({ taskTypes }) => {
                 e.preventDefault();
                 toggleTypeSelection(type.slug as task_slug);
               }}
-              className={`cursor-pointer rounded-full border-none hover:border hover:border-1 text-xs font-medium leading-4 h-5 p-0 px-[10px] ${selectedTypes.includes(type.slug as task_slug)
+              className={`cursor-pointer rounded-full border-none hover:border hover:border-1 text-xs font-medium leading-4 h-5 p-0 px-[10px] ${typeParams.includes(type.slug as task_slug)
                 ? "bg-foreground text-background hover:bg-foreground/50 hover:text-background"
                 : "bg-secondary hover:bg-secondary/50 text-foreground"
                 }`}
@@ -83,24 +100,6 @@ const FilterTypeMenu: FC<FilterTypeMenuProps> = ({ taskTypes }) => {
             </Button>
 
           ))}
-          {/* // <Badge className="text-black bg-gray-100 hover:bg-gray-50">
-          //   Software Dev
-          // </Badge>
-          // <Badge className="text-black bg-gray-100 hover:bg-gray-50">
-          //   Hardware Dev
-          // </Badge>
-          // <Badge className="text-black bg-gray-100 hover:bg-gray-50">
-          //   Legal
-          // </Badge>
-          // <Badge className="text-black bg-gray-100 hover:bg-gray-50">
-          //   Legal
-          // </Badge>
-          // <Badge className="text-black bg-gray-100 hover:bg-gray-50">
-          //   Legal
-          // </Badge>
-          // <Badge className="text-black bg-gray-100 hover:bg-gray-50">
-          //   Legal
-          // </Badge> */}
         </div>
         <Separator className="my-4" />
         <div className="mx-3">
